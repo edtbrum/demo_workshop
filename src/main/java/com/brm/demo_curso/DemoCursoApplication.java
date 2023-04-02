26fecb10c044572496d362e6c20c33f948ef9f13
edtbrum <edtbrum@gmail.com>
2023-04-01 21:32:33 -0300

    Projeto criado

---
+++ src/main/java/com/brm/demo_curso/DemoCursoApplication.java
@@ -0,0 +1,13 @@
+package com.brm.demo_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brm/demo_curso/DemoCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brm.demo_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
