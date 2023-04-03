3a4f5f36d6cecf49fc77975bc2b8dcd925fdb371
edtbrum <edtbrum@gmail.com>
2023-04-03 17:38:46 -0300

    Order Service e Resource

---
+++ src/main/java/com/brm/demo_curso/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.brm.demo_curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.brm.demo_curso.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
